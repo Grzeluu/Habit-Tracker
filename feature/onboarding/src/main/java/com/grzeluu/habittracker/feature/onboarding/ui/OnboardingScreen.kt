3438f930ffb2043084b04b0b66a3cbc0a54a19d5
Grzeluu <patryk.grzelewski6@gmail.com>
2024-10-23 22:11:05 +0200

    Added holt navigation

--- feature/onboarding/src/main/java/com/grzeluu/habittracker/feature/onboarding/ui/OnboardingScreen.kt
+++ feature/onboarding/src/main/java/com/grzeluu/habittracker/feature/onboarding/ui/OnboardingScreen.kt
@@ -6,11 +6,12 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun OnboardingScreen() {
 
-    val viewModel: OnboardingViewModel by viewModels()
+    val viewModel: OnboardingViewModel = hiltViewModel()
 
     Surface(
         modifier = Modifier
